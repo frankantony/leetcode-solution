c0e01eb7abc213f7c2389fb3c8bee7febe818be0
frankantony <fafa12041996@gmail.com>
2022-11-22 11:39:43 -0300

    Solving problem

---
+++ substring_without_repeating_character.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<map>
+
+using namespace std;
+
+int lengthOfLongestSubstring (string s) {
+	int len = s.size(), result = 0, i = 0, j = 0;
+	map<char, int> table;
+	while (j < len) {
+		if (table[s[j]])
+			i = max (i, table[s[j]]);
+            
+		result = max (result, j - i + 1);
+		table[s[j++]] = j + 1;
+	}
+        
+	return result;
+}
+
+
+int main () {
+	string s;
+	cin >> s;
+	int result = lengthOfLongestSubstring (s);
+	cout << result << endl;
+	return 0;
+}
