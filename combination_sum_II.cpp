ec221db7a3029a1ffe32c643466ad02d33b76f9b
frankantony <fafa12041996@gmail.com>
2022-11-25 08:46:18 -0300

    Improving the solution from problem and solving other similar problem involving backtracking

--- combination_sum_II.cpp
+++ combination_sum_II.cpp
@@ -9,26 +9,27 @@ void solve (int i, vector<int>& candidates, int target, vector<vector<int> >& an
 		answer.push_back(back);
 		return;
 	}
-        
+
 	for (int j = i; j < candidates.size() && target - candidates[j] >= 0; j++) {
+		if (j > i && candidates[j] == candidates[j-1])
+			continue;
+            
 		back.push_back(candidates[j]);
             
-		solve (j , candidates, target - candidates[j], answer, back);
+		solve (j + 1, candidates, target - candidates[j], answer, back);
             
 		back.pop_back();
 	}
 }
-    
-vector<vector<int> > combinationSum(vector<int>& candidates, int target) {
-	vector<vector<int> > answer;
-	vector<int> back;
-        
+
+vector<vector<int> > combinationSum2(vector<int>& candidates, int target) {
 	sort(candidates.begin(), candidates.end());
         
+	vector<vector<int> > answer;
+	vector<int> back;
 	solve (0, candidates, target, answer, back);
 	return answer;
 }
-
 
 int main () {
 	int n, value, target;
@@ -40,7 +41,7 @@ int main () {
 	}
 
 	cin >> target;
-	vector <vector <int> > result = combinationSum (candidates, target);
+	vector <vector <int> > result = combinationSum2 (candidates, target);
 	cout << "[";
 	for (int i = 0; i < result.size(); i++) {
 		cout << "[" << result[i][0];
