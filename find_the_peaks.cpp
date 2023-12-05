c46fe172245dc63cdcb03f2fccf9c57437c0407a
frankantony <fafa12041996@gmail.com>
2023-12-05 16:44:21 -0300

    Solving the problem find the peak

---
+++ find_the_peaks.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> findPeaks(vector<int>& mountain) {
+        int len = mountain.size();
+        vector<int> result;
+        for (int i = 1; i < len - 1; i++)
+            if (mountain[i] > mountain[i-1] && mountain[i] > mountain[i+1])
+                result.push_back(i);
+        return result;
+    }
+};
