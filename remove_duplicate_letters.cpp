95ded77bcd050406d227d6c84d35354e4c11fe51
frankantony <fafa12041996@gmail.com>
2024-01-27 11:41:45 -0300

    Solving two identicals questions

---
+++ remove_duplicate_letters.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        vector<int> last(26, 0);
+        vector<bool> flag(26, false);
+        int len = s.size();
+        for (int i = 0; i < len; i++)
+            last[s[i] - 'a'] = i;
+        string result = "";
+        for (int i = 0; i < len; i++) {
+            if (!flag[s[i] - 'a']) {
+                while (!result.empty() && result.back() > s[i] && last[result.back() - 'a'] > i) {
+                    flag[result.back() - 'a'] = 0;
+                    result.pop_back();
+                }
+                result += s[i];
+            }
+            flag[s[i] - 'a'] = true;
+        }
+        return result;
+    }
+};
