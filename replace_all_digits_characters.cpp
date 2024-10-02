4bfacfc6aa1a237fe18f10a179177f06d274eba0
frankantony <fafa12041996@gmail.com>
2024-10-02 17:06:27 -0300

    Solving one question involving characters

---
+++ replace_all_digits_characters.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    string replaceDigits(string s) {
+        int len = s.size();
+        for (int i = 1; i < len; i += 2)
+            s[i] = s[i-1] + (s[i] - '0');
+        return s;
+    }
+};
