3106d012e5aff3ce758895283dac147b3a96a841
frankantony <fafa12041996@gmail.com>
2025-03-09 21:52:26 -0300

    improving the code

--- non_decreasing_array.cpp
+++ non_decreasing_array.cpp
@@ -5,9 +5,7 @@ public:
         bool find = false;
         for (int i = 0; i < len - 1; i++) {
             if (nums[i] > nums[i+1]) {
-                if (find)
-                    return false;
-                if (i >= 1 && nums[i-1] > nums[i+1] && i < len - 2 && nums[i] > nums[i+2])
+                if (find || i >= 1 && nums[i-1] > nums[i+1] && i < len - 2 && nums[i] > nums[i+2])
                     return false;
                 find = true;
             }
