08683973269213560f13cc0878abc8573fbecdda
frankantony <fafa12041996@gmail.com>
2025-05-26 18:58:04 -0300

    Solving two problems involving dynamic programming

---
+++ edit_distance.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.size(), n = word2.size();
+        int dp[m + 1][n + 1];
+        for (int i = 0; i <= m; i++)
+            dp[i][0] = i;
+        for (int i = 0; i <= n; i++)
+            dp[0][i] =  i;
+
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= n; j++) {
+                int value_min = min (dp[i-1][j] + 1, dp[i][j-1] + 1);
+                value_min = min (value_min, dp[i-1][j-1] + (word1[i-1] != word2[j-1]));
+                dp[i][j] = value_min;
+            }
+        }
+
+        return dp[m][n];
+    }
+};
